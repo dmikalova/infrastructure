fcd60fc6a17b6794e3dc5de95f05205c85452f83
David Mikalova <324548+dmikalova@users.noreply.github.com>
2026-09-24 21:48:05 -0700

    chore: migrate github provider to integrations/github

--- terraform/modules/github/repositories/main.tf
+++ terraform/modules/github/repositories/main.tf
@@ -1,6 +1,15 @@
 # GitHub repositories module
 # Creates and manages GitHub repositories for a single owner
 
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
+
 resource "github_repository" "repos" {
   for_each = var.repositories
 
@@ -72,7 +81,7 @@ locals {
 resource "github_actions_secret" "deploy_secrets" {
   for_each = local.repo_secret_keys
 
-  plaintext_value = var.secrets[each.value.name]
+  value = var.secrets[each.value.name]
   repository      = github_repository.repos[each.value.repo].name
   secret_name     = each.value.name
 }
